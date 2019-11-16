9e8c7a28973c1606456711bc61806c702674d080
szefoski <szefoski22@gmail.com>
2019-11-16 13:45:19 +0100

    moved business logic to a service

--- src/main/java/com/daniel/lotto/LottoController.java
+++ src/main/java/com/daniel/lotto/LottoController.java
@@ -1,32 +1,22 @@
 package com.daniel.lotto;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 @Controller
 public class LottoController {
-    private Random random = new Random(System.currentTimeMillis());
 
-    private ArrayList<Integer> generateNumbers(int amountOfNumbers) {
-        ArrayList<Integer> bucket = new ArrayList<>();
-
-        for (int i = 0; i < amountOfNumbers; ++i) {
-            bucket.add(random.nextInt(49) + 1);
-        }
-
-        return bucket;
-    }
+    @Autowired
+    private LottoGeneratorService service;
 
     @GetMapping("/generate-lotto")
     @ResponseBody
     public String generateLotto(@RequestParam("id") String amount) {
         try {
-            return generateNumbers(Integer.parseInt(amount)).toString();
+            return service.generateNumbers(Integer.parseInt(amount)).toString();
         } catch (NumberFormatException e) {
             return "Wrong 'amount' parameter value:</br>" + amount;
         }
---
+++ src/main/java/com/daniel/lotto/LottoGeneratorService.java
@@ -0,0 +1,21 @@
+package com.daniel.lotto;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LottoGeneratorService {
+    private Random random = new Random(System.currentTimeMillis());
+
+    public ArrayList<Integer> generateNumbers(int amountOfNumbers) {
+        ArrayList<Integer> bucket = new ArrayList<>();
+
+        for (int i = 0; i < amountOfNumbers; ++i) {
+            bucket.add(random.nextInt(49) + 1);
+        }
+
+        return bucket;
+    }
+}
