98c955f5a9e4e7ebc163fd43bc867f0d58f28441
szefoski <szefoski22@gmail.com>
2019-11-17 15:52:10 +0100

    added new api for JSON data input and path variable

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ This application generates Lotto numbers (from 1 to 49).
 ## Usage
 * http://localhost:8080/generate-lotto?id=6
 * http://localhost:8080/generate-lotto?id=12311
+* http://localhost:8080/generate/{number}
+* http://localhost:8080/welcome/{name}
+* curl --header "Content-Type: application/json" --request POST --data '[13,8,19,3,45,33]' http://localhost:8080/check-wins/
--- src/main/java/com/daniel/lotto/LottoController.java
+++ src/main/java/com/daniel/lotto/LottoController.java
@@ -1,12 +1,22 @@
 package com.daniel.lotto;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class LottoController {
 
     @Autowired
@@ -21,4 +31,23 @@ public class LottoController {
             return "Wrong 'amount' parameter value:</br>" + amount;
         }
     }
+
+    @GetMapping("/generate/{amount}")
+    public ResponseEntity<List<Integer>> generate(@PathVariable("amount") int amount) {
+        return new ResponseEntity<List<Integer>>(service.generateNumbers(amount), HttpStatus.OK);
+    }
+
+    @GetMapping("/welcome/{name}")
+    public ResponseEntity<HashMap<String, String>> generate2(@PathVariable("name") String name) {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("Super person is", name);
+        data.put("Have a great one", name);
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+	@RequestMapping(value = "/check-wins/", method = RequestMethod.POST)
+	public ResponseEntity<?> updateUser(@RequestBody ArrayList<Integer> numbers) {
+        //TODO check if given numbers won in the past
+		return new ResponseEntity<String>("Given numbers: " + numbers.toString(), HttpStatus.OK);
+	}
 }
