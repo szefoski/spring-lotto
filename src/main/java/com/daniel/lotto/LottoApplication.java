6fbda0b8183e82d45040705429ffe1c826a8e623
szefoski <szefoski22@gmail.com>
2019-12-27 16:54:30 +0100

    Added cache of games archive

--- src/main/java/com/daniel/lotto/LottoApplication.java
+++ src/main/java/com/daniel/lotto/LottoApplication.java
@@ -2,8 +2,12 @@ package com.daniel.lotto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableCaching
+@Configuration
 public class LottoApplication {
 
 	public static void main(String[] args) {
